fc4740ea86ddb6a06433c366555f50fbbd5d5746
Deivid <deividfloresvgs@gmail.com>
2026-07-02 09:40:48 -0500

    fix: remover declaracion duplicada de cors

--- src/server.js
+++ src/server.js
@@ -11,8 +11,6 @@ const taskRoutes = require('./routes/taskRoutes');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
-const cors = require('cors');
 app.use(cors({
   origin: '*', 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
